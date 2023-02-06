f91bcafb3525f3945efad79ee54a0d212d7a7880
bugattie <m.mohsinbarkaati@gmail.com>
2023-02-07 01:45:36 +0500

    dark mode

--- src/components/_App/Navbar.js
+++ src/components/_App/Navbar.js
@@ -31,76 +31,75 @@ const Navbar = () => {
     : "navbar-toggler navbar-toggler-right"
 
   return (
-    <header id="header" className="headroom">
-      <div className="startp-nav">
-        <div className="container">
-          <nav className="navbar navbar-expand-md navbar-light">
-            <Link to="/" onClick={toggleNavbar} className="navbar-brand">
-              <img src={Logo} alt="logo" />
-            </Link>
+    <>
+      <header id="header" className="headroom">
+        <div className="startp-nav">
+          <div className="container">
+            <nav className="navbar navbar-expand-md navbar-light">
+              <Link to="/" onClick={toggleNavbar} className="navbar-brand">
+                <img src={Logo} alt="logo" />
+              </Link>
 
-            <button
-              onClick={toggleNavbar}
-              className={classTwo}
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="icon-bar top-bar"></span>
-              <span className="icon-bar middle-bar"></span>
-              <span className="icon-bar bottom-bar"></span>
-            </button>
+              <button
+                onClick={toggleNavbar}
+                className={classTwo}
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="icon-bar top-bar"></span>
+                <span className="icon-bar middle-bar"></span>
+                <span className="icon-bar bottom-bar"></span>
+              </button>
 
-            <div className={classOne} id="navbarSupportedContent">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <Link to="/process" className="nav-link">
-                    Process
-                  </Link>
-                </li>
+              <div className={classOne} id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <Link to="/process" className="nav-link">
+                      Process
+                    </Link>
+                  </li>
 
-                <li className="nav-item">
-                  <Link
-                    to="/projects"
-                    className="nav-link"
-                    activeClassName={"active"}
-                  >
-                    Projects
-                  </Link>
-                </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/projects"
+                      className="nav-link"
+                      activeClassName={"active"}
+                    >
+                      Projects
+                    </Link>
+                  </li>
 
-                <li className="nav-item">
-                  <Link
-                    to="/insights"
-                    className="nav-link"
-                    activeClassName={"active"}
-                  >
-                    Insights
-                  </Link>
-                </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/insights"
+                      className="nav-link"
+                      activeClassName={"active"}
+                    >
+                      Insights
+                    </Link>
+                  </li>
 
-                <li className="nav-item">
-                  <Link
-                    to="/team"
-                    className="nav-link"
-                    activeClassName={"active"}
-                  >
-                    Team
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <DarkModeToggle />
-                </li>
-              </ul>
-            </div>
-          </nav>
+                  <li className="nav-item">
+                    <Link
+                      to="/team"
+                      className="nav-link"
+                      activeClassName={"active"}
+                    >
+                      Team
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <DarkModeToggle />
+    </>
   )
 }
 
