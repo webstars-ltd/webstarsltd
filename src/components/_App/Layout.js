f9028841b0d8a4acdcbd735c7764cb2fc3141aae
bugattie <m.mohsinbarkaati@gmail.com>
2023-02-07 01:48:28 +0500

    dark mode

--- src/components/_App/Layout.js
+++ src/components/_App/Layout.js
@@ -1,11 +1,11 @@
 import React from "react"
-import GoTop from "./GoTop"
+import DarkModeToggle from "./DarkModeToggle"
 
 const Layout = ({ children }) => {
   return (
     <>
       {children}
-      <GoTop scrollStepInPx="100" delayInMs="10.50" />
+      <DarkModeToggle />
     </>
   )
 }
