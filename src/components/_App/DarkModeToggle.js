f37e20b8bdd18c7a9515ba9d21716121b802279e
bugattie <m.mohsinbarkaati@gmail.com>
2023-02-07 01:52:35 +0500

    dark mode

--- src/components/_App/DarkModeToggle.js
+++ src/components/_App/DarkModeToggle.js
@@ -20,7 +20,7 @@ const DarkModeToggle = () => {
         }}
       >
         <div className="toggle-icon">
-          {darkMode.value ? (
+          {!darkMode.value ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
