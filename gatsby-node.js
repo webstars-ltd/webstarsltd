b394679dfc867646abcad8750cff9c2a7934a91f
bugattie <m.mohsinbarkaati@gmail.com>
2023-02-11 15:29:06 +0500

    dark mode theme styling

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,18 +6,18 @@ exports.createPages = async ({ graphql, actions }) => {
   const projectTemplate = path.resolve("./src/templates/SingleProject.js")
   const insightTemplate = path.resolve("./src/templates/SingleInsight.js")
 
-  // const res = await graphql(`
-  //   query {
-  //     allStoryblokEntry(filter: { field_component: { eq: "SingleProject" } }) {
-  //       edges {
-  //         node {
-  //           field_component
-  //           slug
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const res = await graphql(`
+    query {
+      allStoryblokEntry(filter: { field_component: { eq: "SingleProject" } }) {
+        edges {
+          node {
+            field_component
+            slug
+          }
+        }
+      }
+    }
+  `)
 
   // const insightQueryResponse = await graphql(`
   //   query {
@@ -32,15 +32,15 @@ exports.createPages = async ({ graphql, actions }) => {
   //   }
   // `)
 
-  // res?.data?.allStoryblokEntry.edges.forEach(edge => {
-  //   createPage({
-  //     component: projectTemplate,
-  //     path: `/projects/${edge?.node?.slug}`,
-  //     context: {
-  //       slug: edge?.node?.slug,
-  //     },
-  //   })
-  // })
+  res?.data?.allStoryblokEntry.edges.forEach(edge => {
+    createPage({
+      component: projectTemplate,
+      path: `/projects/${edge?.node?.slug}`,
+      context: {
+        slug: edge?.node?.slug,
+      },
+    })
+  })
 
   // insightQueryResponse?.data?.allStoryblokEntry.edges.forEach(edge => {
   //   createPage({
