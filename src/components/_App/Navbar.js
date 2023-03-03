a86ee0f9121fc67b85042ebe5f7f375b50e45890
bugattie <m.mohsinbarkaati@gmail.com>
2023-03-04 01:23:06 +0500

    css fix

--- src/components/_App/Navbar.js
+++ src/components/_App/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
   })
 
   React.useEffect(() => {
-    if (typeof window === "undefined") {
+    if (window) {
       const dark = JSON.parse(localStorage.getItem("darkMode"))
       console.log(dark)
       setValue(dark)
