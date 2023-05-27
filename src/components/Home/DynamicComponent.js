b6b3c8ba9c4d0a22dcdf8db9c2cd0371c66c543b
bugattie <m.mohsinbarkaati@gmail.com>
2023-05-27 15:06:30 +0500

    blogs on landing page changes

--- src/components/Home/DynamicComponent.js
+++ src/components/Home/DynamicComponent.js
@@ -13,6 +13,7 @@ const Components = {
 }
 
 const DynamicComponent = ({ blok }) => {
+  console.log(blok)
   if (typeof Components[blok.component] !== "undefined") {
     const Component = Components[blok.component]
     return <Component blok={blok} key={blok._uid} />
