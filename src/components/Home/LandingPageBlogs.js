01625055e0dab316b0bd79a63d35723914227c6f
bugattie <m.mohsinbarkaati@gmail.com>
2023-05-27 16:00:44 +0500

    blogs on landing page changes

--- src/components/Home/LandingPageBlogs.js
+++ src/components/Home/LandingPageBlogs.js
@@ -1,27 +1,28 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, StaticQuery } from "gatsby"
 
 const LandingPageBlogs = ({ blok }) => {
     console.log(blok)
+    const title = blok.blog_one
 
-    let blog_one = useStaticQuery(graphql`
-    query {
-      story: allStoryblokEntry(
-        filter: { name: { eq: ${blok.blog_one} } }
-      ) {
-        edges {
-          node {
-            field_component
-            slug
-            uuid
-            id
-            name
-            content
-          }
-        }
-      }
-    }
-  `)
+//     let blog_one = useStaticQuery(graphql`
+//     query {
+//       story: allStoryblokEntry(
+//         filter: { name: { eq: ${blok.blog_one} } }
+//       ) {
+//         edges {
+//           node {
+//             field_component
+//             slug
+//             uuid
+//             id
+//             name
+//             content
+//           }
+//         }
+//       }
+//     }
+//   `)
 
     //   let blog_two = useStaticQuery(graphql`
     //   query {
@@ -45,7 +46,32 @@ const LandingPageBlogs = ({ blok }) => {
     console.log(blog_one)
 
     return (
-        <div>LandingPageBlogs</div>
+        <StaticQuery query={graphql`
+  query HeadingQuery($title: String!) {
+    story: allStoryblokEntry(
+        filter: { name: { eq: $title } }
+      ) {
+        edges {
+          node {
+            field_component
+            slug
+            uuid
+            id
+            name
+            content
+          }
+        }
+      }
+    }
+  `
+        }
+            variables={{ title }}
+            render={data => (
+                <div>
+                    <div>Component {console.log(data)}</div>
+                </div>
+            )}
+        />
     )
 }
 
