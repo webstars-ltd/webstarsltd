a8c729f3fbb9066ac9c59aab1b15cd114958cb8f
bugattie <m.mohsinbarkaati@gmail.com>
2023-05-27 15:41:22 +0500

    blogs on landing page changes

--- src/components/Home/LandingPageBlogs.js
+++ src/components/Home/LandingPageBlogs.js
@@ -4,10 +4,10 @@ import { useStaticQuery, graphql } from "gatsby"
 const LandingPageBlogs = ({ blok }) => {
     console.log(blok)
 
-    let { story } = useStaticQuery(graphql`
+    let blog_one = useStaticQuery(graphql`
     query {
       story: allStoryblokEntry(
-        filter: { name: { eq: ${blog_one} } }
+        filter: { name: { eq: ${blok.blog_one} } }
       ) {
         edges {
           node {
@@ -23,7 +23,26 @@ const LandingPageBlogs = ({ blok }) => {
     }
   `)
 
-    console.log(story)
+    //   let blog_two = useStaticQuery(graphql`
+    //   query {
+    //     story: allStoryblokEntry(
+    //       filter: { name: { eq: ${blok.blog_two} } }
+    //     ) {
+    //       edges {
+    //         node {
+    //           field_component
+    //           slug
+    //           uuid
+    //           id
+    //           name
+    //           content
+    //         }
+    //       }
+    //     }
+    //   }
+    // `)
+
+    console.log(blog_one)
 
     return (
         <div>LandingPageBlogs</div>
