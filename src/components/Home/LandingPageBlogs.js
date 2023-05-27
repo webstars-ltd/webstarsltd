77ddeda9c960f8c044f91e5063ee78babe4e5694
bugattie <m.mohsinbarkaati@gmail.com>
2023-05-27 16:44:16 +0500

    blogs on landing page changes

--- src/components/Home/LandingPageBlogs.js
+++ src/components/Home/LandingPageBlogs.js
@@ -1,78 +1,15 @@
 import React from 'react'
 import { useStaticQuery, graphql, StaticQuery } from "gatsby"
 
+import QueryComponent from './QueryComponent'
+
 const LandingPageBlogs = ({ blok }) => {
     console.log(blok)
     const title = blok.blog_one
 
-//     let blog_one = useStaticQuery(graphql`
-//     query {
-//       story: allStoryblokEntry(
-//         filter: { name: { eq: ${blok.blog_one} } }
-//       ) {
-//         edges {
-//           node {
-//             field_component
-//             slug
-//             uuid
-//             id
-//             name
-//             content
-//           }
-//         }
-//       }
-//     }
-//   `)
+    console.log(title)
 
-    //   let blog_two = useStaticQuery(graphql`
-    //   query {
-    //     story: allStoryblokEntry(
-    //       filter: { name: { eq: ${blok.blog_two} } }
-    //     ) {
-    //       edges {
-    //         node {
-    //           field_component
-    //           slug
-    //           uuid
-    //           id
-    //           name
-    //           content
-    //         }
-    //       }
-    //     }
-    //   }
-    // `)
-
-    console.log(blog_one)
-
-    return (
-        <StaticQuery query={graphql`
-  query HeadingQuery($title: String!) {
-    story: allStoryblokEntry(
-        filter: { name: { eq: $title } }
-      ) {
-        edges {
-          node {
-            field_component
-            slug
-            uuid
-            id
-            name
-            content
-          }
-        }
-      }
-    }
-  `
-        }
-            variables={{ title }}
-            render={data => (
-                <div>
-                    <div>Component {console.log(data)}</div>
-                </div>
-            )}
-        />
-    )
+    return <QueryComponent title={title} />
 }
 
 export default LandingPageBlogs
