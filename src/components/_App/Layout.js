3f80187ac3f042f46afcef558e80cd6f6674ccbe
bugattie <m.mohsinbarkaati@gmail.com>
2023-06-03 14:26:03 +0500

    css fix

--- src/components/_App/Layout.js
+++ src/components/_App/Layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import DarkModeToggle from "./DarkModeToggle"
+import { Script } from "gatsby"
 
 const Layout = ({ children }) => {
   return (
     <>
       {children}
       <DarkModeToggle />
+      <Script id="privasee-cb" src="https://app.privasee.io/cdn/63628705d67de9001332dd71/cookie-banner.js" />
     </>
   )
 }
