f5e978093fe5e4ad31e5de55b6f67577d4d82abd
bugattie <m.mohsinbarkaati@gmail.com>
2023-06-18 02:25:25 +0500

    list component

--- src/components/Insights/InsightList.js
+++ src/components/Insights/InsightList.js
@@ -1,7 +1,6 @@
 import React from "react"
 
-// import ListComponent from "./ListComponent"
-import TestFile from "./testFile"
+import ListComponent from "./ListComponent"
 
 const InsightLists = ({ showMore, categoryTitle }) => {
   return (
@@ -9,34 +8,19 @@ const InsightLists = ({ showMore, categoryTitle }) => {
       <div className="insight-display ptb-40 insight-display-list">
         <div className="container">
           <div className="row">
-            {showMore
-              .filter((originalStory, index) => (
-                <TestFile
-                  key={index}
-                  story={originalStory.node}
-                  index={index}
-                  categoryTitle={categoryTitle}
-                />
-              ))
-              .map((item, index) => (
-                <div
-                  className="col-md-6 col-lg-6"
-                  key={index}
-                  id="undefined-content"
-                >
-                  {item}
-                </div>
-              ))}
-
-            {/* {showMore.filter((originalStory, index) => (
-              
+            {showMore.map((originalStory, index) => (
+              <div
+                className="col-md-6 col-lg-6"
+                key={index}
+                id="undefined-content"
+              >
                 <ListComponent
                   story={originalStory.node}
                   index={index}
                   categoryTitle={categoryTitle}
                 />
               </div>
-            ))} */}
+            ))}
           </div>
         </div>
       </div>
