3f683f0c64d0dc13fb594b07fed6bb962184fbfa
bugattie <m.mohsinbarkaati@gmail.com>
2023-06-20 03:42:57 +0500

    contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 // import NativeForms from "native-forms-react"
 
 // App level import statements
@@ -10,37 +10,51 @@ import Footer from "../components/_App/Footer"
 const NativeForms = React.lazy(() => import("native-forms-react"))
 
 const ContactUs = () => {
-  useEffect(() => {
-    if (typeof window === "undefined" || !window.document) {
-      return
-    }
-  }, [])
-
-  return (
-    <Layout>
-      <SEO
-        title="Contact Us"
-        description={
-          "Webstars are experts in everything digital - from design and development, to strategy and ongoing support, we are a full-service agency here to bring your digital aspirations to life."
-        }
-      />
-      <Navbar />
-      {/* Body Component Starts here */}
-      <div className="contact-us-container">
-        <React.Suspense fallback={<></>}>
-          {(typeof window === "undefined" || !window.document) && (
-            <NativeForms
-              form="https://form.nativeforms.com/JZDdV1jZm80UPJnWH1Db"
-              onClose={result => console.log("on-close", result)}
-              onSend={result => console.log("on-send", result?.form?.completed)}
-            />
-          )}
-        </React.Suspense>
-      </div>
-      {/* Body Component Ends here */}
-      <Footer />
-    </Layout>
-  )
+  if (typeof window !== "undefined") {
+    return (
+      <Layout>
+        <SEO
+          title="Contact Us"
+          description={
+            "Webstars are experts in everything digital - from design and development, to strategy and ongoing support, we are a full-service agency here to bring your digital aspirations to life."
+          }
+        />
+        <Navbar />
+        {/* Body Component Starts here */}
+        <div className="contact-us-container">
+          <React.Suspense fallback={<></>}>
+            {(typeof window === "undefined" || !window.document) && (
+              <NativeForms
+                form="https://form.nativeforms.com/JZDdV1jZm80UPJnWH1Db"
+                onClose={result => console.log("on-close", result)}
+                onSend={result =>
+                  console.log("on-send", result?.form?.completed)
+                }
+              />
+            )}
+          </React.Suspense>
+        </div>
+        {/* Body Component Ends here */}
+        <Footer />
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <SEO
+          title="Contact Us"
+          description={
+            "Webstars are experts in everything digital - from design and development, to strategy and ongoing support, we are a full-service agency here to bring your digital aspirations to life."
+          }
+        />
+        <Navbar />
+        {/* Body Component Starts here */}
+        <div className="contact-us-container" />
+        {/* Body Component Ends here */}
+        <Footer />
+      </Layout>
+    )
+  }
 }
 
 export default ContactUs
