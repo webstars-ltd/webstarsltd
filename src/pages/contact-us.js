747f615d29d2616cb1854b57780a0c9a62ae5ace
bugattie <m.mohsinbarkaati@gmail.com>
2023-06-19 23:58:13 +0500

    contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -6,7 +6,7 @@ import SEO from "../components/_App/seo"
 import Navbar from "../components/_App/Navbar"
 import Footer from "../components/_App/Footer"
 
-const NativeForms = React.lazy(() => import("native-forms-react"))
+// const NativeForms = React.lazy(() => import("native-forms-react"))
 
 const ContactUs = () => {
   return (
@@ -19,13 +19,13 @@ const ContactUs = () => {
       />
       <Navbar />
       {/* Body Component Starts here */}
-      <React.Suspense fallback={<></>}>
-        <NativeForms
-          form="https://form.nativeforms.com/JZDdV1jZm80UPJnWH1Db"
-          onClose={result => console.log("on-close", result)}
-          onSend={result => console.log("on-send", result?.form?.completed)}
-        />
-      </React.Suspense>
+      {/* <React.Suspense fallback={<></>}> */}
+      <NativeForms
+        form="https://form.nativeforms.com/JZDdV1jZm80UPJnWH1Db"
+        onClose={result => console.log("on-close", result)}
+        onSend={result => console.log("on-send", result?.form?.completed)}
+      />
+      {/* </React.Suspense> */}
       {/* Body Component Ends here */}
       <Footer />
     </Layout>
