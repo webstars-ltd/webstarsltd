dbc2476e85056f925050e1aa21e58c2c49549cc4
bugattie <m.mohsinbarkaati@gmail.com>
2023-11-26 17:30:56 +0500

    wistia video

--- src/components/SingleProject/BannerImage.js
+++ src/components/SingleProject/BannerImage.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { storyblokEditable } from "@storyblok/js"
+import { WistiaProvider, WistiaPlayer } from "@wistia/react-embeds"
 
 const BannerImage = ({ blok }) => {
   return (
     <div {...storyblokEditable(blok)}>
       <section className="single-projectSec1">
         <div className="custom-container">
-          <div className="singl-project-sec1-img">
-            <img src={blok.banner_img.filename} alt="" />
-          </div>
+          {blok && blok.wistia_video_id ? (
+            <WistiaProvider>
+              <WistiaPlayer hashedId={blok.wistia_video_id} />
+            </WistiaProvider>
+          ) : (
+            <div className="singl-project-sec1-img">
+              <img src={blok.banner_img.filename} alt="" />
+            </div>
+          )}
         </div>
       </section>
     </div>
