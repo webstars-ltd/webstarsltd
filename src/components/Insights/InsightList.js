663db859080836dec84e3457245aa1814c44586e
bugattie <m.mohsinbarkaati@gmail.com>
2024-01-09 18:01:04 +0500

    layout fixed

--- src/components/Insights/InsightList.js
+++ src/components/Insights/InsightList.js
@@ -4,27 +4,25 @@ import ListComponent from "./ListComponent"
 
 const InsightLists = ({ showMore, categoryTitle }) => {
   return (
-    <>
-      <div className="insight-display ptb-40 insight-display-list">
-        <div className="container">
-          <div className="row">
-            {showMore.map((originalStory, index) => (
-              <div
-                className="col-md-6 col-lg-6"
-                key={index}
-                id="undefined-content"
-              >
-                <ListComponent
-                  story={originalStory}
-                  index={index}
-                  categoryTitle={categoryTitle}
-                />
-              </div>
-            ))}
-          </div>
+    <div className="insight-display ptb-40 insight-display-list">
+      <div className="container">
+        <div className="row">
+          {showMore.map((originalStory, index) => (
+            <div
+              className="col-md-6 col-lg-6"
+              key={index}
+              id="undefined-content"
+            >
+              <ListComponent
+                story={originalStory}
+                index={index}
+                categoryTitle={categoryTitle}
+              />
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/Insights/InsightLists.js
+++ src/components/Insights/InsightLists.js
@@ -3,30 +3,26 @@ import React from "react"
 
 const InsightLists = ({ blok, slug, index }) => {
   return (
-    <>
-      {
-        <Link
-          to={`/insights/${slug}`}
-          style={{ marginBottom: 50 }}
-          className="insights-link-space"
-        >
-          <div
-            className={
-              index % 2 === 0 ? `insight-display-one` : `insight-display-two`
-            }
-            style={{ backgroundColor: blok.insights[0].background_hex_code }}
-          >
-            <h6>{blok.insights[0].tag}</h6>
-            <h4>{blok.insights[0].title}</h4>
-            <img
-              src={blok.insights[0].image.filename}
-              alt="Insight"
-              className="insight-display-one-img"
-            />
-          </div>
-        </Link>
-      }
-    </>
+    <Link
+      to={`/insights/${slug}`}
+      style={{ marginBottom: 50 }}
+      className="insights-link-space"
+    >
+      <div
+        className={
+          index % 2 === 0 ? `insight-display-one` : `insight-display-two`
+        }
+        style={{ backgroundColor: blok.insights[0].background_hex_code }}
+      >
+        <h6>{blok.insights[0].tag}</h6>
+        <h4>{blok.insights[0].title}</h4>
+        <img
+          src={blok.insights[0].image.filename}
+          alt="Insight"
+          className="insight-display-one-img"
+        />
+      </div>
+    </Link>
   )
 }
 
