7d1cd76ba024285b0de1151a5bf1d4b7fd733681
bugattie <m.mohsinbarkaati@gmail.com>
2024-02-09 16:25:32 +0500

    env var fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: "gatsby-source-storyblok",
       options: {
-        accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
+        accessToken: "ptJYKVleibu33Rg3xo6qUAtt",
         version: "published",
         localAssets: true,
       },
