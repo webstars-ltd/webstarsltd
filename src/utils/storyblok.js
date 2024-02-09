18022cb32589022a01f8a962b17f929e157bfb3a
bugattie <m.mohsinbarkaati@gmail.com>
2024-02-09 15:56:11 +0500

    env var fixed

--- src/utils/storyblok.js
+++ src/utils/storyblok.js
@@ -6,7 +6,7 @@ const sbConfig = config.plugins.find(
 )
 
 const { storyblokApi } = storyblokInit({
-  accessToken: sbConfig.options.accessToken,
+  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
   use: [apiPlugin],
 })
 
