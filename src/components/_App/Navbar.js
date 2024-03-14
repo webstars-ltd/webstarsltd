cf3c26010e87c7ab96c2a56da7e1aa12a931d20c
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2024-03-14 09:13:15 +0000

    Nav menu text change

--- src/components/_App/Navbar.js
+++ src/components/_App/Navbar.js
@@ -97,7 +97,7 @@ const Navbar = () => {
                     className="nav-link"
                     activeClassName={"active"}
                   >
-                    Services
+                    Products
                   </Link>
                 </li>
 
